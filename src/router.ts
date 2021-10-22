be24dabaace346ab5239bcbe309c4da9063ce78d
Kian Mayne <git@kianmayne.co.uk>
2021-10-22 20:34:48 +0100

    Enable Vue router history mode

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import VueRouter from 'vue-router';
 import MainPage from './components/MainPage.vue';
 
-export default new VueRouter({ routes: [
-  { path: '/', component: MainPage },
-]});
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: MainPage },
+  ]
+});
