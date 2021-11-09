2974826891a63f7c25623f76cfb8d88657ae7cf9
Kian Mayne <git@kianmayne.co.uk>
2021-11-09 09:34:49 +0000

    Default edges to be directional

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -118,7 +118,7 @@ export default Vue.extend({
       this.selectedEdge = null;
       this.liveEdge = {
         id: this.graph.generateId(),
-        direction: EdgeDirection.Undirected,
+        direction: EdgeDirection.Directional,
         fromId: id, toId: '',
         fromX: x, fromY: y,
         toX: x, toY: y
